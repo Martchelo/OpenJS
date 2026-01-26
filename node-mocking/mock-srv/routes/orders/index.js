0684189d1a5a706999eec1a7fcf3658350c5eb43
Marcelo Reis <mr.marceloric@gmailcom>
2026-01-25 21:27:43 -0300

    Adicionada implementação da nova rota utilizando os geradores e com ajustes de lógica em variáveis.

--- node-mocking/mock-srv/routes/orders/index.js
+++ node-mocking/mock-srv/routes/orders/index.js
@@ -4,8 +4,27 @@ module.exports = async function (fastify, opts) {
   fastify.get(
      '/:category',
     { websocket: true },
-    async (conn, request) => {
-      conn.send(JSON.stringify({ id: 'A1', total: 3 }))
+
+    //conn.socket === undefined
+    //In this case, changes follow.
+
+    async (conn, request) => { //replaced 'socket' to 'conn'
+
+      let open = true //created for control
+
+      //created API
+      conn.on('close', () => {
+        open = false
+      })
+
+      for (const order of fastify.currentOrders(request.params.category)) {
+        if (!open) return //inserted to control
+        conn.send(order) //replaced 'socket' to 'conn'
+      }
+      for await (const order of fastify.realtimeOrders()) {
+        if (!open) break //replaced for "if (socket.readyState >= socket.CLOSING) break"
+        conn.send(order) //replaced 'socket' to 'conn'
+      }
     }
   )
 }
