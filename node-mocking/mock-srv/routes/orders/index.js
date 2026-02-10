ed2a9ed6af3602c6bc95ac6500ac1ec5ea225799
Marcelo Reis <mr.marceloric@gmail.com>
2026-02-10 11:34:08 -0300

    Adicionadas duas funções para lidar com comunicação bidirecional

--- node-mocking/mock-srv/routes/orders/index.js
+++ node-mocking/mock-srv/routes/orders/index.js
@@ -1,6 +1,27 @@
 'use strict'
 
 module.exports = async function (fastify, opts) {
+  function monitorMessages(socket) {
+    socket.on('message', (data) => {
+      try {
+        const { cmd, payload } = JSON.parse(data)
+        if (cmd === 'update-category') {
+          sendCurrentOrders(payload.category, socket)
+        }
+      } catch (err) {
+        fastify.log.warn(
+          'WebSocket Message (data: %o) Error: %s',
+          data,
+          err.message
+        )
+      }
+    })
+  }
+  function sendCurrentOrders(category, socket) {
+    for (const order of fastify.currentOrders(category)) {
+      socket.send(order)
+    }
+  }
   fastify.get(
      '/:category',
     { websocket: true },
@@ -12,19 +33,27 @@ module.exports = async function (fastify, opts) {
 
       let open = true //created for control
 
+      monitorMessages(conn)
+      sendCurrentOrders(request.params.category, conn)
+      for await (const order of fastify.realtimeOrders()) {
+        if (!open) break //replaced for "if (socket.readyState >= socket.CLOSING) break"
+        conn.send(order) //replaced 'socket' to 'conn'
+      }
+
       //created API
       conn.on('close', () => {
         open = false
       })
 
-      for (const order of fastify.currentOrders(request.params.category)) {
+      //Discontinued!
+      /* for (const order of fastify.currentOrders(request.params.category)) {
         if (!open) return //inserted to control
         conn.send(order) //replaced 'socket' to 'conn'
       }
       for await (const order of fastify.realtimeOrders()) {
         if (!open) break //replaced for "if (socket.readyState >= socket.CLOSING) break"
         conn.send(order) //replaced 'socket' to 'conn'
-      }
+      } */
     }
   )
 }
